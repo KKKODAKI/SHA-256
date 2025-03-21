e320859508af5f32175283b298e1cbe55f00442c
KKKODAKI <l.tieppo10@gmail.com>
2025-03-21 17:01:31 -0300

    Message schedule feito

--- encode.py
+++ encode.py
@@ -3,6 +3,34 @@ letras_bin = ''
 size = ''
 size = bin(len(original[:-1]))
 size = size[2:] + '000'
+w = []
+
+
+def rightRotate(n_bin, rot):
+    n = int(n_bin, 2)
+    final_bin = n_bin[-(rot):]
+    numRot = n >> rot
+
+    numRot_bin = bin(numRot)
+    numRot_bin = numRot_bin[2:]
+    while(len(numRot_bin) != 32):
+        numRot_bin = '0' + numRot_bin
+    numRot_bin = final_bin + numRot_bin[rot:]
+
+    return numRot_bin
+
+def soma(w0, sigma0, w9, sigma1, i):
+    novo_bin = bin(w0 + sigma0 + w9 + sigma1)
+
+    novo_bin = novo_bin[2:]
+
+    if(len(novo_bin) > 32):
+        novo_bin = novo_bin[-32:]
+    while(len(novo_bin) != 32):
+        novo_bin = '0' + novo_bin 
+
+    return novo_bin
+
 
 for i in range(len(original)):
     letras = format(ord(original[i]), 'b')
@@ -30,16 +58,26 @@ while((len(original) + 64) % 512 != 0):
 
 original = original + size
 
-'''
-binario_arrumado = ''
-for i in range(len(original)):
-    if(i % 8 == 0):
-        if(i % 32 == 0):
-            binario_arrumado = binario_arrumado + '\n'
-        binario_arrumado = binario_arrumado + ' ' + original[i:(i+8)]
-print(binario_arrumado)
-'''
 
-print(original)
-        
+for i in range(len(original)):
+    if(i % 32 == 0):
+        w.append(original[i:(i+32)])
+
+
+while(len(w) != 64):
+    w.append('00000000000000000000000000000000')
+
+
+for i in range(16,len(w)):
+    sigma0 = (int(rightRotate(w[i-15],7), 2) ^ int(rightRotate(w[i-15],18), 2)) ^ (int(w[i-15], 2) >> 3)
+    
+
+    sigma1 = (int(rightRotate(w[i-2],17), 2) ^ int(rightRotate(w[i-2],19), 2)) ^ ((int(w[i-2],2)) >> 10)
+    
+ 
+    w[i] = soma(int(w[i-16],2), sigma0, int(w[i-7],2), sigma1, i)
+
+
+print(w)
+
 
